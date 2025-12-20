0f125010359e167e34515abb47e2e83466f37536
Lee0701 <hgl1002@naver.com>
2025-12-21 03:56:21 +0900

    Add global redis options

--- Server/lib/Web/db.js
+++ Server/lib/Web/db.js
@@ -39,7 +39,10 @@ const FAKE_REDIS = {
 };
 
 Pub.ready = function(isPub){
-	var Redis	 = require("redis").createClient();
+	var Redis	 = require("redis").createClient({
+		host: GLOBAL.REDIS_HOST,
+		port: GLOBAL.REDIS_PORT
+	});
     var Pg = new PgPool({
         user: GLOBAL.PG_USER,
         password: GLOBAL.PG_PASSWORD,
